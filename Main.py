76512725f05f24638081930090003fc5d63138c6
Tiques1 <tiques13@mail.ru>
2024-02-13 01:25:56 +0500

    Up to 75% precision

--- Main.py
+++ Main.py
@@ -2,14 +2,7 @@ import keyboard
 from pynput.mouse import Listener
 import threading
 import PointCheckerOptimized as pc
-# import win32process
-# import win32api
-# import win32con
-#
-# pid = win32api.GetCurrentProcessId()
-# handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, False, pid)
-#
-# win32process.SetPriorityClass(handle, win32process.REALTIME_PRIORITY_CLASS)
+
 
 flag = True
 
@@ -33,27 +26,6 @@ def start_listener():
     flag = True
     threading.Thread(target=thread_generator, args=[min(a[0], b[0]), min(a[1], b[1]),
                                                             max(a[0], b[0]), max(a[1], b[1])]).start()
-
-
-def bresenham_line(x0, y0, x1, y1):
-    points = []
-    dx = abs(x1 - x0)
-    dy = abs(y1 - y0)
-    sx = 1 if x0 < x1 else -1
-    sy = 1 if y0 < y1 else -1
-    err = dx - dy
-
-    while x0 != x1 or y0 != y1:
-        points.append((x0, y0))
-        err2 = 2 * err
-        if err2 > -dy:
-            err -= dy
-            x0 += sx
-        if err2 < dx:
-            err += dx
-            y0 += sy
-    points.append((x0, y0))
-    return points
 
 
 def thread_generator(x1, y1, x2, y2):
--- PointCheckerOptimized.py
+++ PointCheckerOptimized.py
@@ -1,22 +1,16 @@
 import numpy as np
-from PIL import ImageGrab
 import pyautogui
 
-#min_colors = np.array([(15, 230, 190), (15, 215, 110), (150, 100, 220)])
-#max_colors = np.array([(50, 250, 255), (30, 245, 130), (210, 130, 255)])
-
-min_colors = np.array([(0, 150, 100), (0, 150, 100), (150, 50, 150)])
-max_colors = np.array([(100, 250, 255), (80, 245, 150), (210, 130, 255)])
+min_colors = np.array([(15, 220, 190), (15, 215, 110), (150, 100, 220)])
+max_colors = np.array([(170, 255, 255), (150, 245, 180), (210, 130, 255)])
 
 keys = ['up', 'left', 'f']
 key_states = {key: False for key in keys}
 
 
 def checker(x1, y1, x2, y2):
-    #img = ImageGrab.grab(bbox=(x1, y1, x2, y2))
     img = pyautogui.screenshot(region=(x1, y1, x2-x1, y2-y1))
     pixels = np.array(img)
-    #pixels = np.array([img.getpixel((x, y)) for x, y in points])
     for i in range(len(keys)):
         mask = np.all((min_colors[i] <= pixels) & (pixels <= max_colors[i]), axis=2)
         if np.any(mask):
