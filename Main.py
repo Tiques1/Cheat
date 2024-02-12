6fb5605aa7f42328ac577d8f5f1fc3cb5bb2a4d1
Tiques1 <tiques13@mail.ru>
2024-02-12 22:01:31 +0500

    Delete excess functions

--- Main.py
+++ Main.py
@@ -1,26 +1,57 @@
 import keyboard
 from pynput.mouse import Listener
-import draw_line as dl
 import threading
+import time
+import PointChecker
+
+flag = True
 
 
 def start_listener():
     a, b = None, None
+    global flag
+    flag = False
 
     def on_click(x, y, button, pressed):
         nonlocal a, b
         if pressed:
             a = [x, y]
-            # print(f'Нажата кнопка {button} в точке: x={x}, y={y}')
         else:
-            # print(f'Отпущена кнопка {button} в точке: x={x}, y={y}')
             b = [x, y]
             listener.stop()
 
     with Listener(on_click=on_click) as listener:
         listener.join()
 
-    threading.Thread(target=dl.line, args=(a, b)).start()
+    flag = True
+    threading.Thread(target=thread_generator, args=bresenham_line(a[0], a[1], b[0], b[1])).start()
+
+
+def bresenham_line(x0, y0, x1, y1):
+    points = []
+    dx = abs(x1 - x0)
+    dy = abs(y1 - y0)
+    sx = 1 if x0 < x1 else -1
+    sy = 1 if y0 < y1 else -1
+    err = dx - dy
+
+    while x0 != x1 or y0 != y1:
+        points.append((x0, y0))
+        err2 = 2 * err
+        if err2 > -dy:
+            err -= dy
+            x0 += sx
+        if err2 < dx:
+            err += dx
+            y0 += sy
+    points.append((x0, y0))
+    return points
+
+
+def thread_generator(points):
+    while flag:
+        threading.Thread(target=PointChecker.checker, args=points).start()
+        time.sleep(10)
 
 
 keyboard.add_hotkey('ctrl+alt+q', start_listener)
--- PointChecker.py
+++ PointChecker.py
@@ -1,58 +1,30 @@
 from PIL import ImageGrab
 import pyautogui
 
-min_blue = (0, 190, 235)
-max_blue = (50, 255, 250)
-min_green = (0, 220, 120)
-max_green = (5, 255, 130)
-min_violet = (170, 100, 240)
-max_violet = (230, 125, 255)
+# min_blue = (0, 190, 235)
+# max_blue = (50, 255, 250)
+# min_green = (0, 220, 120)
+# max_green = (5, 255, 130)
+# min_violet = (170, 100, 240)
+# max_violet = (230, 125, 255)
 
 
-def checker(a, b):
-    points = bresenham_line(a[0], a[1], b[0], b[1])
-    while True:
-        img = ImageGrab.grab()
-        for x, y in points:
-            color = img.getpixel((x, y))
-            if is_color_in_range(color, min_blue, max_blue):
-                pyautogui.keyDown('up')
-            else:
-                pyautogui.keyUp('up')
-            if is_color_in_range(color, min_green, max_green):
-                pyautogui.keyDown('left')
-            else:
-                pyautogui.keyUp('left')
-            if is_color_in_range(color, min_violet, max_violet):
-                pyautogui.keyDown('f')
-            else:
-                pyautogui.keyUp('f')
-
-
-def is_color_in_range(pixel_color, min_color, max_color):
-    r, g, b = pixel_color
-    min_r, min_g, min_b = min_color
-    max_r, max_g, max_b = max_color
-
-    return (min_r <= r <= max_r) and (min_g <= g <= max_g) and (min_b <= b <= max_b)
-
-
-def bresenham_line(x0, y0, x1, y1):
-    points = []
-    dx = abs(x1 - x0)
-    dy = abs(y1 - y0)
-    sx = 1 if x0 < x1 else -1
-    sy = 1 if y0 < y1 else -1
-    err = dx - dy
-
-    while x0 != x1 or y0 != y1:
-        points.append((x0, y0))
-        err2 = 2 * err
-        if err2 > -dy:
-            err -= dy
-            x0 += sx
-        if err2 < dx:
-            err += dx
-            y0 += sy
-    points.append((x0, y0))
-    return points
+def checker(points):
+    img = ImageGrab.grab()
+    for x, y in points:
+        color = img.getpixel((x, y))
+        if (0 <= color[0] <= 50) and (190 <= color[1] <= 255) \
+                and (235 <= color[2] <= 250):
+            pyautogui.keyDown('up')
+        else:
+            pyautogui.keyUp('up')
+        if (0 <= color[0] <= 5) and (220 <= color[1] <= 255) \
+                and (120 <= color[2] <= 130):
+            pyautogui.keyDown('left')
+        else:
+            pyautogui.keyUp('left')
+        if (170 <= color[0] <= 230) and (100 <= color[1] <= 125) \
+                and (240 <= color[2] <= 255):
+            pyautogui.keyDown('f')
+        else:
+            pyautogui.keyUp('f')
